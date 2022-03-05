2f511164881eaacb1919bbec9dc87bf53b2f8a23
Yunxiai88 <goalking019@gmail.com>
2022-03-05 11:42:03 +0800

    add

--- __init__.py
+++ __init__.py
@@ -1,4 +1,4 @@
-from mycroft import MycroftSkill, intent_file_handler
+from mycroft import MycroftSkill, intent_file_handler, intent_handler
 
 
 class EasyShopping(MycroftSkill):
@@ -8,6 +8,17 @@ class EasyShopping(MycroftSkill):
     @intent_file_handler('shopping.easy.intent')
     def handle_shopping_easy(self, message):
         self.speak_dialog('shopping.easy')
+    
+    @intent_handler('is.there.any.goods.intent')
+    def handle_is_there_any_goods(self, message):
+        category = message.data.get('category')
+        result = 'yes, i find ' + category + ' in front of you'
+        self.speak(result)
+    
+    @intent_handler('view.goods.intent')
+    def handle_view_goods(self, message):
+        self.speak('Taking a photo now. Please wait a second for me to get the result.')
+        self.speak('I find some goods here, you can ask me whatever goods you want.')
 
 
 def create_skill():
